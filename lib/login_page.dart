e27f7606914035e5e063b697daf60d542d2279c7
Fatih YİLMAZ <fatihyilmazzz95@gmail.com>
2022-06-01 00:52:06 +0300

    login with shared preferences

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,12 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class LoginPage extends StatelessWidget {
+import 'package:webroutingtest/main.dart';
+import 'package:webroutingtest/model/login_data.dart';
+
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late TextEditingController _textController;
+  @override
+  void initState() {
+    super.initState();
+
+    _textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("login"),
+    return Scaffold(
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+                controller: _textController,
+                decoration: InputDecoration(
+                  hintText: "e-mail",
+                  prefixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.email,
+                    ),
+                  ),
+                )),
+            const SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "password",
+                prefixIcon: IconButton(
+                  onPressed: () async {},
+                  icon: const Icon(
+                    Icons.lock,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                context
+                    .read<LoginData>()
+                    .saveEmailToSharedPref(_textController.text);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MyDataPage()));
+              },
+              child: const Text("GO"),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:webroutingtest/api_data.dart';
+import 'package:webroutingtest/login_page.dart';
+import 'package:webroutingtest/model/login_data.dart';
 import 'package:webroutingtest/route/build_route_map.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await LoginData().createPrefObject();
   setUrlStrategy(PathUrlStrategy());
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(providers: [
+      ChangeNotifierProvider<ApiData>(
+        create: (context) => ApiData(),
+      ),
+      ChangeNotifierProvider<LoginData>(
+        create: (context) => LoginData(),
+      ),
+    ], child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,40 +31,51 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ApiData(),
-      child: MaterialApp.router(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        routerDelegate: RoutemasterDelegate(
-          routesBuilder: (context) {
-            return buildRouteMap(context);
-          },
-        ),
-        routeInformationParser: const RoutemasterParser(),
-        routeInformationProvider: routeInformationProvider,
+    context.read<LoginData>().loadEmailFromSharedPref();
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      routerDelegate: RoutemasterDelegate(
+        routesBuilder: (context) {
+          return buildRouteMap(context);
+        },
+      ),
+      routeInformationParser: const RoutemasterParser(),
+      routeInformationProvider: routeInformationProvider,
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyDataPage extends StatelessWidget {
+  const MyDataPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     context.read<ApiData>().fetchData();
     return Scaffold(
       appBar: AppBar(
-        title: const Text("HELLO!"),
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              context.read<LoginData>().deleteEmailFromSharedPref();
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext ctx) => const LoginPage()));
+            },
+          ),
+        ],
       ),
       body: RefreshIndicator(
         child: Consumer<ApiData>(
           builder: (context, value, child) {
             return value.users.isEmpty
-                ? const CircularProgressIndicator()
+                ? const Center(child: CircularProgressIndicator())
                 : ListView.builder(
                     itemCount: value.users.length,
                     itemBuilder: (context, index) {
